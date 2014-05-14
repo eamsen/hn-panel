eb354535ace60512700c434ceb9b4e8aa4b3f18f
Eugen Sawin <esawin@me73.com>
2014-05-14 17:04:38 +0200

    Reduced image size and added caching.

--- node-rndpng.js
+++ node-rndpng.js
@@ -1,8 +1,9 @@
 "use strict"
 
-var http = require("http");
-var url = require("url");
-var png = require("png").Png;
+var http = require("http"),
+    url = require("url"),
+    png = require("png").Png,
+    cache = require("memory-cache");
 
 function hash(s) {
   var key = 0;
@@ -17,25 +18,32 @@ function hash(s) {
 http.createServer(function(req, res) {
   var request = url.parse(req.url, true);
   var action = request.pathname.substr(1);
-  var width = 16;
-  var height = 16;
+  var width = 1;
+  var height = 1;
+  var timeout = 30 * 60 * 1000;
 
   var key = hash(action);
-  var red = (key ^ 55818801) % 255;
-  var green = (key ^ 30023737) % 255;
-  var blue = (key ^ 54382664) % 255;
+  var image_data = cache.get(key);
 
-  var buffer = new Buffer(width * height * 3);
-  for (var i = 0; i < height; ++i) {
-    for (var j = 0; j < width; ++j) {
-      buffer[i * width * 3 + j * 3 + 0] = red;
-      buffer[i * width * 3 + j * 3 + 1] = green;
-      buffer[i * width * 3 + j * 3 + 2] = blue;
+  if (!image_data) {
+    var red = (key ^ 55818801) % 255;
+    var green = (key ^ 30023737) % 255;
+    var blue = (key ^ 54382664) % 255;
+
+    var buffer = new Buffer(width * height * 3);
+    for (var i = 0; i < height; ++i) {
+      for (var j = 0; j < width; ++j) {
+        buffer[i * width * 3 + j * 3 + 0] = red;
+        buffer[i * width * 3 + j * 3 + 1] = green;
+        buffer[i * width * 3 + j * 3 + 2] = blue;
+      }
     }
+    var image = new png(buffer, width, height, "rgb").encodeSync();
+    image_data = image.toString("binary");
+    cache.put(key, image_data, timeout);
   }
-  var image = new png(buffer, width, height, "rgb").encodeSync();
   res.writeHead(200, {"Content-Type": "image/png"});
-  res.end(image.toString("binary"), "binary");
-}).listen(8080, "127.0.0.1");
+  res.end(image_data, "binary");
+}).listen(84, "127.0.0.1");
 
 console.log("rndpng running at localhost:84");
